419ccd0046af59b0c87159fb09a89de1bf9a3f4b
pbragas <99732794+pbragas@users.noreply.github.com>
2022-05-02 13:51:34 +0200

    Create register.php

---
+++ HDTELH/register.php
@@ -0,0 +1,21 @@
+<form name="inscription" method="post" action="">
+   <fieldset>
+      <legend><h2>Inscription</h2></legend>
+      Nom<br>
+      <input type="text" name="nom" value=""><br>
+      Prénom<br>
+      <input type="text" name="prenom" value=""><br>
+      Email<br>
+      <input type="text" name="email" value=""><br>
+      Login<br>
+      <input type="text" name="login" value=""><br>
+      Mot de passe<br>
+      <input type="password" name="pass" value=""><br>
+      Vous êtes<br>
+      <select name="niv">
+         <option>Admin</option>
+         <option>Lecteur</option>
+      </select><br>
+      <input type="button" name="envoyer" value="S'inscrire">
+   </fieldset>
+</form>
